06b41feb37f3ec8ea2fe71bc0640b6121a05a88e
Felipe David <felipedavid.huh@gmail.com>
2021-07-31 18:48:23 -0300

    compiling every example

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-# Observação: Quase nunca uso python e queria acabar isso o mais rápido possível..
-# Então pode esperar um código de ótima "qualidade", haha :v
 
 import sys
 import re
 
-type_r = {"ADD":0, "AND":4, "NAND":1, "NOR":7, "OR":5, "SLT":2, "SUB":3}
-type_i = {"ADDI":8, "BEQ":4, "LW":3, "SW":0xa}
-
-labels = []
+type_r = {
+    "ADD":  0x0000, 
+    "AND":  0x0004, 
+    "NAND": 0x0001, 
+    "NOR":  0x0007, 
+    "OR":   0x0005,  
+    "SLT":  0x0002, 
+    "SUB":  0x0003,
+}
+type_i = {
+    "ADDI": 0x8000, 
+    "BEQ":  0x4000, 
+    "LW":   0x3000, 
+    "SW":   0xa000,
+}
 
 def main():
     if len(sys.argv) != 2:
@@ -18,62 +27,61 @@ def main():
     as_file = open(sys.argv[1], "r")
     bi_file = open("a.out", "w")
 
-    ibuffer = ["v2.0 raw\n"]
+    ibuffer = []
 
     addr = 0
     for i in as_file:
         tok = list(filter(None, re.split(",| ", i)))
         iname = tok[0].upper()
-        machine_i = ""
 
-        print(tok)
-        if iname in type_r.keys():
-            ibuffer.append(encode_r_instruction(tok, iname) + '\n')
+        if iname in type_r:
+            ibuffer.append(encode_r_instruction(tok, iname, addr))
         elif iname in type_i:
-            ibuffer.append(encode_i_instruction(tok, iname) + '\n')
+            ibuffer.append(encode_i_instruction(tok, iname, addr))
         elif iname == "J":
-            ibuffer.append(encode_j_instruction(tok) + '\n')
-        else:
-            printf("ERROR:" + iname + "is not a valid instruction.\n")
-            return
+            ibuffer.append(0x2000 | int(tok[1].strip(), 0))
+
         addr += 2
-    
+
+    bi_file.write("v2.0 raw\n")
     for i in ibuffer:
-        bi_file.write(i)
-
-def bin_string(value: int, padding: int) -> str:
-    return format(int(value), '0'+str(padding)+'b')
-
-def encode_r_instruction(tokens: list, iname: str) -> str:
-    opc = bin_string(0, 4)
-    rs = bin_string(tokens[2][1], 3)
-    rt = bin_string(tokens[3][1], 3)
-    rd = bin_string(tokens[1][1], 3)
-    funct = bin_string(type_r[iname], 3)
-
-    bin_instruction = opc + rs + rt + rd + funct
-    return format(int(bin_instruction, 2), '04x')
+        bi_file.write(format(i, '04x') + '\n')
     
-def encode_i_instruction(tokens: list, iname: str) -> str:
-    opc = bin_string(type_i[iname], 4)
 
-    if iname == "BEQ":
-        rt = bin_string(tokens[2][1], 3)
-        rs = bin_string(tokens[1][1], 3)
-        immd = bin_string(int(tokens[3].strip(), 0), 6)
+def encode_r_instruction(tokens: list, iname: str, addr: int) -> int:
+    rs = int(tokens[2][1])
+    rt = int(tokens[3][1])
+    rd = int(tokens[1][1])
+
+    mi = type_r[iname]
+    v_rang = range(0, 7)
+
+    if rs in v_rang and rt in v_rang and rd in v_rang:
+        mi |= (rs << 9)
+        mi |= (rt << 6)
+        mi |= (rd << 3)
     else:
-        rt = bin_string(tokens[1][1], 3)
-        rs = bin_string(tokens[2].split("$", 1)[1][0], 3)
-        immd = bin_string(tokens[2].split("$", 1)[0].replace("(", ""), 6)
+        print(f"ERRO: Linha {addr}. Registrador inválido")
+        exit()
 
-    bin_instruction = opc + rs + rt + immd
-    return format(int(bin_instruction, 2), '04x')
+    return mi;
 
-def encode_j_instruction(tokens: list) -> str:
-    opc = bin_string(2, 4)
-    immd = bin_string(int(tokens[1].strip(), 0), 12)
-    bin_instruction = opc + immd
-    return format(int(bin_instruction, 2), '04x')
+def encode_i_instruction(tokens: list, iname: str, addr: int) -> int:
+    if iname == "BEQ":
+        rt = int(tokens[2][1])
+        rs = int(tokens[1][1])
+        immd = int(tokens[3].strip(), 0)
+    else:
+        rt = int(tokens[1][1])
+        rs = int(tokens[2].split("$", 1)[1][0])
+        immd = int(tokens[2].split("$", 1)[0].replace("(", ""))
+
+    mi = type_i[iname]
+    mi |= (rs << 9)
+    mi |= (rt << 6)
+    mi |= immd
+
+    return mi
 
 if __name__ == "__main__":
     main()
