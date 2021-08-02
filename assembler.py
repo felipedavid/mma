c1b0ab128306c0ade13db94a94dae03388e9c634
Felipe David <felipedavid.huh@gmail.com>
2021-08-02 16:42:17 -0300

    .

--- assembler.py
+++ assembler.py
@@ -28,34 +28,32 @@ def main():
         return
 
     as_file = open(sys.argv[1], "r")
-    bi_file = open("a.out", "w")
 
+    parse_labels(as_file)
+
+    as_file.seek(0)
     ibuffer = []
-
-    addr = 0
+    line = 0
     for i in as_file:
         tok = list(filter(None, re.split(",| ", i)))
         tok[-1] = tok[-1].strip()
         iname = tok[0].upper()
 
-        if len(tok) == 1 and tok[0][-1] == ':':
-            tok[0] = tok[0].replace(":", "")
-            labels[tok[0]] = addr + 2
-        elif iname in type_r:
-            ibuffer.append(encode_r_instruction(tok, iname, addr))
+        if iname in type_r:
+            ibuffer.append(encode_r_instruction(tok, iname, line))
         elif iname in type_i:
-            ibuffer.append(encode_i_instruction(tok, iname, addr))
+            ibuffer.append(encode_i_instruction(tok, iname, line))
         elif iname == "J":
-            ibuffer.append(encode_j_instruction(tok, iname, addr))
+            ibuffer.append(encode_j_instruction(tok, iname, line))
 
-        addr += 2
+        line += 1
 
+    bi_file = open("a.out", "w")
     bi_file.write("v2.0 raw\n")
     for i in ibuffer:
         bi_file.write(format(i, '04x') + '\n')
     
-
-def encode_r_instruction(tokens: list, iname: str, addr: int) -> int:
+def encode_r_instruction(tokens: list, iname: str, line: int) -> int:
     rs = int(tokens[2][1])
     rt = int(tokens[3][1])
     rd = int(tokens[1][1])
@@ -68,15 +66,19 @@ def encode_r_instruction(tokens: list, iname: str, addr: int) -> int:
         mi |= (rt << 6)
         mi |= (rd << 3)
     else:
-        print(f"ERRO: Linha {addr}. Registrador inválido")
+        print(f"ERRO: Linha {line}. Registrador inválido")
         exit()
 
     return mi;
 
-def encode_i_instruction(tokens: list, iname: str, addr: int) -> int:
+def encode_i_instruction(tokens: list, iname: str, line: int) -> int:
     if iname == "BEQ":
-        rt = int(tokens[2][1])
         rs = int(tokens[1][1])
+        rt = int(tokens[2][1])
+        immd = int(tokens[3].strip(), 0)
+    elif iname == "ADDI":
+        rs = int(tokens[2][1])
+        rt = int(tokens[1][1])
         immd = int(tokens[3].strip(), 0)
     else:
         rt = int(tokens[1][1])
@@ -91,12 +93,22 @@ def encode_i_instruction(tokens: list, iname: str, addr: int) -> int:
     return mi
 
 
-def encode_j_instruction(tokens: list, iname: str, addr: int) -> int:
+def encode_j_instruction(tokens: list, iname: str, line: int) -> int:
     if (tokens[1][0].isalpha()):
         tokens[1] = labels[tokens[1]]
     else:
         tokens[1] = tokens[1].strip()
     return 0x2000 | tokens[1]
+
+
+def parse_labels(file):
+    line = 0
+    for i in file:
+        l = i.strip()
+        line += 1
+        if l[-1] == ':':
+            labels[l.replace(":", "")] = line
+
 
 if __name__ == "__main__":
     main()
