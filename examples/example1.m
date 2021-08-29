e4e2cc9504dd83777e82c02d6d05557016a4fa36
Felipe David <felipedavid.huh@gmail.com>
2021-08-28 22:10:38 -0300

    modifying examples

--- examples/example1.m
+++ examples/example1.m
@@ -1,9 +1,23 @@
-.data:
-    .word 0x5
-    .word 0xA
-
-.text:
-    lw $2, 0($1)
-    lw $3, 2($1)
-    add $r4, $r2, $r3
-    sw $4, 4($1)
+.data
+  _f: .word 0
+  _g: .word 5
+  _h: .word 4
+  _i: .word 2
+  _j: .word 2 
+.text
+  #Carrega os valores das vari�veis nos registradores MEM -> REG
+  lw $r1, _g
+  lw $r2, _h
+  lw $r3, _i
+  lw $r4, _j
+  #Realiza o calculo
+  bne $r3,$r4,Else  #se i!=j => Else
+  add $r0,$r1,$r2   #f=g+h (se i!=j pula, se i==j executa) 
+  j Exit            #salto incondicional para Exit
+  Else:
+  sub $r0,$r1,$r2   #se i!=j, f=g-h  
+  Exit:
+  #Retorna o valor do resultado para a mem�ria REG -> MEM
+  sw $r0, _f
+  #Termina o programa
+  li $r0, 10
