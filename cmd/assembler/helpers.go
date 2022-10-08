5e13f2e4124e11bb0b0438464fd2b25c99b2b1b7
Felipe David <felipedavid.huh@gmail.com>
2022-10-08 01:22:26 -0300

    Starting rewrite

--- cmd/assembler/helpers.go
+++ cmd/assembler/helpers.go
@@ -1,24 +1,33 @@
 package main
 
-func isSpace(b byte) bool {
-	return b == ' ' || b == '\t' || b == '\n' || b == '\r' || b == '\v'
+func isWhiteSpace(ch byte) bool {
+	return ch == ' ' || ch == '\t' || ch == '\r' || ch == '\v'
 }
 
-func isDigit(b byte) bool {
-	return b >= '0' && b <= '9'
+func isDigit(ch byte) bool {
+	return ch >= '0' && ch <= '9'
 }
 
-func isLower(b byte) bool {
-	return b >= 'a' && b <= 'z'
+func isLower(ch byte) bool {
+	return ch >= 'a' && ch <= 'z'
 }
 
-func toLower(b byte) byte {
-	if isLower(b) {
-		return b
+func isUpper(ch byte) bool {
+	return ch >= 'A' && ch <= 'Z'
+}
+
+func isLetter(ch byte) bool {
+	return isLower(ch) || isUpper(ch)
+}
+
+func isAlphaNumeric(ch byte) bool {
+	return isLetter(ch) || isDigit(ch)
+
+}
+
+func toLower(ch byte) byte {
+	if isUpper(ch) {
+		ch += 32
 	}
-	return b + 32
-}
-
-func isAlphaNumeric(b byte) bool {
-	return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || isDigit(b)
+	return ch
 }
